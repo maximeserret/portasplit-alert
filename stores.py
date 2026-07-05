ad8cf388e7003b7fe1a902b3fd146894bc8ef62f
maximeserret <maxime.serret@icloud.com>
2026-07-05 15:44:42 +0200

    Update stores.py

--- stores.py
+++ stores.py
@@ -1,16 +1,7 @@
-import random
-
 def search_all(product_name: str):
-    """
-    Simulation de stock (remplacera ensuite le vrai scraping)
-    """
-
-    if random.random() < 0.25:  # 25% chance dispo
-        return {
-            "name": product_name,
-            "price": random.randint(850, 950),
-            "url": f"https://example.com/{product_name.replace(' ', '-').lower()}",
-            "store": "DemoStore"
-        }
-
-    return None
+    return {
+        "name": product_name,
+        "price": 899,
+        "url": "https://example.com/test-product",
+        "store": "TEST_STORE"
+    }
